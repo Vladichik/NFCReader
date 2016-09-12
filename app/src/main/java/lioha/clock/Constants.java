f3fd67968e482932f0a74f40b39d6973be2309ab
Vladichik <vvidavsky@gmail.com>
2016-09-12 20:51:21 +0300

    Update Constants.java

--- app/src/main/java/lioha/clock/Constants.java
+++ app/src/main/java/lioha/clock/Constants.java
@@ -4,7 +4,7 @@ package lioha.clock;
  * Created by vladvidavsky on 6/7/16.
  */
 public class Constants {
-    public static final String BASE_URL = "http://54.245.121.216:8500";
+    public static final String BASE_URL = "";
     public static final String RESURRECT_STRING = "RESURRECT";
 
 }
